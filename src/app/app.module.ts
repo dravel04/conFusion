71e8536ccc9bd9b3bb9498d538c6225ffcfd9d9f
Rafael Antonio Marin Sanchez <rafamasa91@gmail.com>
2020-10-09 22:23:58 +0200

    Add header and footer, also include font-awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,16 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
+    HeaderComponent,
+    FooterComponent,
     DishdetailComponent
   ],
   imports: [
